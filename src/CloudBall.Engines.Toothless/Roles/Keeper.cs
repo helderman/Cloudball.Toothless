86db7b157ea7c7bf4fc5a2c7f205de7425a2b862
Corniel <mail@corniel.nl>
2015-06-25 15:50:57 +0200

    Implemented a not that bad keeper.

--- src/CloudBall.Engines.Toothless/Roles/Keeper.cs
+++ src/CloudBall.Engines.Toothless/Roles/Keeper.cs
@@ -1,15 +1,60 @@
-﻿using System;
+﻿using CloudBall.Engines.Toothless.Models;
+using Common;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace CloudBall.Engines.Toothless.Roles
 {
 	public class Keeper : IRole
 	{
-		public Common.Player Apply(Models.TurnInfo turn, IEnumerable<Common.Player> queue)
+		private static readonly Vector DefaultPositon = new Vector(150, Field.MyGoal.Center.Y);
+
+		public Player Apply(TurnInfo turn, IEnumerable<Player> queue)
 		{
-			return null;
+			var keeper = GetClosestBy(queue);
+
+			if (keeper != null)
+			{
+				var catchup = turn.CatchUps.FirstOrDefault();
+
+				// give it just a best try.
+				if (catchup == null)
+				{
+					keeper.ActionGo(turn.Ball);
+				}
+				else 
+				{
+					var vector = (catchup.Position - Field.MyGoal.Center);
+					vector.Normalize();
+
+					var target = Field.MyGoal.Center + vector * 150;
+					keeper.ActionGo(target);
+				}
+			}
+			return keeper;
+		}
+
+		private Player GetClosestBy(IEnumerable<Player> queue)
+		{
+			return queue
+				.OrderBy(q => GetDistanceToGoalSquared(q.Position))
+				.FirstOrDefault();
+		}
+
+		public float GetDistanceToGoalSquared(Vector position)
+		{
+			// above the goal
+			if (position.Y <= Field.EnemyGoal.Top.Y)
+			{
+				return (Field.EnemyGoal.Top - position).LengthSquared;
+			}
+			if (position.Y >= Field.EnemyGoal.Bottom.Y)
+			{
+				return (Field.EnemyGoal.Bottom - position).LengthSquared;
+			}
+			var dY = Field.Borders.Y - position.Y;
+
+			return dY * dY;
 		}
 	}
 }
