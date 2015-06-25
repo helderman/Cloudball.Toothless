2d99c0d10af720199278a5e973bf3afaac3809f2
Ruud Helderman <r.helderman@hccnet.nl>
2015-06-25 11:47:53 +0200

    Just dumb running and shooting

--- src/CloudBall.Engines.Toothless/Bot.cs
+++ src/CloudBall.Engines.Toothless/Bot.cs
@@ -12,7 +12,22 @@ namespace CloudBall.Engines.Toothless
 	{
 		public void Action(Team myTeam, Team enemyTeam, Ball ball, MatchInfo matchInfo)
 		{
-			
+			foreach (Player player in myTeam.Players)
+			{
+				if (ball.Owner == player)
+				{
+					player.ActionShootGoal();
+				}
+				else
+				{
+					player.ActionGo(ball);
+					if (player.CanPickUpBall(ball))
+					{
+						player.ActionPickUpBall();
+					}
+				}
+				//player.ActionGo(new Vector(50, 50));
+			}
 		}
 	}
 }
