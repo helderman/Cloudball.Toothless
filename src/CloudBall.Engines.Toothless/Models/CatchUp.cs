52c113dd4044e549ddb3ae6b0bf3d34e5738a6b6
Corniel <mail@corniel.nl>
2015-06-25 14:07:47 +0200

    add cachup

---
+++ src/CloudBall.Engines.Toothless/Models/CatchUp.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+
+namespace CloudBall.Engines.Toothless.Models
+{
+	public class CatchUp
+	{
+		public Player Player { get; set; }
+		public int Turn { get; set; }
+		public Vector Position { get; set; }
+	}
+}
